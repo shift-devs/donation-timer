bd077acd5a88bb29cf8c50e495f5a1928db54564
darkrta <dark@dark.ski>
2023-08-06 21:56:04 -0400

    Changed timer style and added new google fonts. Timer now has a slight glow and uses a mono font. It is also always light on dark.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,23 +8,23 @@ import CurrentTimeBonus from "./pages/CurrentTimeBonus";
 import CurrentDollarBonus from "./pages/DollarBonuses";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import Timer from "./Timer";
 
-const App: React.FC = () => {
-	return (
-		<Router>
-			<ChakraProvider>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/widget' element={<Widget />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/settings' element={<Settings />} />
-					<Route path='/hypetimer' element={<HypeTimer />} />
-					<Route path='/timetiers' element={<CurrentTimeBonus />} />
-					<Route path='/dollartime' element={<CurrentDollarBonus />} />
-				</Routes>
-			</ChakraProvider>
-		</Router>
-	);
-};
+const App: React.FC = () => (
+	<Router>
+		<ChakraProvider>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/test' element={<Timer input_seconds={12342}/>} />
+				<Route path='/widget' element={<Widget />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/settings' element={<Settings />} />
+				<Route path='/hypetimer' element={<HypeTimer />} />
+				<Route path='/timetiers' element={<CurrentTimeBonus />} />
+				<Route path='/dollartime' element={<CurrentDollarBonus />} />
+			</Routes>
+		</ChakraProvider>
+	</Router>
+);
 
 export default App;
