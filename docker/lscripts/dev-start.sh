b65a88d21be5e2083c486e7708989bb6a77f0a8f
Stephen <sbuonamassa@tiberius.com>
2026-06-03 13:05:22 -0400

    fix: better dev experience with hotloading

--- docker/lscripts/dev-start.sh
+++ docker/lscripts/dev-start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 shdir=$(dirname "$0")
-docker compose -f "$shdir/../dev.yml" up -d
-read -rsp $'Press any key to continue...\n' -n 1
+envfile="$shdir/../.env"
+if [ ! -f "$envfile" ]; then
+    cp "$shdir/../.env_template" "$envfile"
+    echo "Created docker/.env from template. Edit it to set CLIENT_ID for authorized mode."
+fi
+docker compose --env-file "$envfile" -f "$shdir/../dev.yml" up --build "$@"
--- docker/wscripts/dev-start.bat
+++ docker/wscripts/dev-start.bat
@@ -1,4 +1,4 @@
 @echo off
 
-docker compose -f "%~dp0/../dev.yml" up -d
-pause
+if not exist "%~dp0/../.env" copy "%~dp0/../.env_template" "%~dp0/../.env"
+docker compose --env-file "%~dp0/../.env" -f "%~dp0/../dev.yml" up --build %*
